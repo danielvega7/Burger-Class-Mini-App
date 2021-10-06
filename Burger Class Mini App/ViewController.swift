ceb114787c20082f506c025425af04b6329ad9a7
Daniel Vega <drvega7@icloud.com>
2021-10-05 19:00:54 -0500

    Initial Commit

---
+++ Burger Class Mini App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Burger Class Mini App
+//
+//  Created by Daniel Vega on 10/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
